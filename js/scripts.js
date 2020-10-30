d88c78e17fe6787087aacfa10a70167c6c6811b4
Bess Campbell <bess.k.campbell@gmail.com>
2020-10-30 13:55:15 -0700

    Show that I now have a working function that I can continue to test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,19 @@
 // let inputNumber = $(parseInt("input#number")).val();
 let inputNumber =10
 let inputArray = [];
+let output= " "
 
 for (i = 0; i <= inputNumber; i++) {
   inputArray.push(i);
 };
-let digits
-let splitDigits
-const roboSpeak = inputArray.map(function digitize(number){ 
-  digits= number.toString();
-  let splitDigits= digits.split();
-  return splitDigits;
-  console.log(splitDigits);
-  console.log(digits);
-  
-  let joinedDigits = splitDigits.join();
-  splitDigits.toEach(function roboger(element){
-    if (splitDigits==="1") {
-    roboSpeak.push("Beep!");
-    } else{
-    roboSpeak.push();
-    }
-  });
-  
+
+const roboSpeak = inputArray.map(function roboger(number){
+  let string= number.toString();
+  if (string.includes("1")){
+      output="Beep!";
+  } else {
+      output= number
+  }
+    
+  return output;
 });
-
-//   let newString= string.replace(/1{1,}/g, "Beep!");
-//   roboSpeak.push(newString);
-//   console.log(roboSpeak);
-// });
-
-
-
-
-
-
- 
-
-
- // for (i = 0; i <= inputArray.length; i++) 
-//   if (roboNumbers.includes(inputArray[i])) {
-//     inputArray[i] = "Beep!"
-//   };
-//   console.log(inputArray);
-
-
-
-
-//   const roboNumbers= [1];
-// let roboSpeak=[];
-// inputArray.forEach(roboger(element) {
-//     inputArray[i].replace(/1{1,}/g, "Beep!")
-//   }
-// });
