03995e9e5dac67b39543054af7a14d1605c9c020
Bess Campbell <bess.k.campbell@gmail.com>
2020-10-30 11:25:33 -0700

    Show that spec 2 is passing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // let inputNumber = $(parseInt("input#number")).val();
-let inputNumber =1
+let inputNumber =10
 let inputArray = [];
 
 for (i = 0; i <= inputNumber; i++) {
@@ -7,27 +7,32 @@ for (i = 0; i <= inputNumber; i++) {
   };
   console.log(inputArray);
  const roboNumbers= [1];
-for (i = 0; i <= inputArray.length; i++) 
-  if (roboNumbers.includes(inputArray[i])) {
-    inputArray[i] = "Beep!"
-  };
-  console.log(inputArray);
+  let roboSpeak=[]
+
+  inputArray.forEach(function roboger(element){ 
+   if (roboNumbers.includes(element)){
+     roboSpeak.push("Beep!")
+   } else {
+     roboSpeak.push(element);
+   }
+console.log(roboSpeak);
+  }); 
+   
+ 
+
+
+ // for (i = 0; i <= inputArray.length; i++) 
+//   if (roboNumbers.includes(inputArray[i])) {
+//     inputArray[i] = "Beep!"
+//   };
+//   console.log(inputArray);
 
 
 
 
-  // const roboNumbers= [1];
+//   const roboNumbers= [1];
 // let roboSpeak=[];
 // inputArray.forEach(roboger(element) {
 //     inputArray[i].replace(/1{1,}/g, "Beep!")
 //   }
 // });
-
-
-
-  // for (i = 0; i <= inputArray.length; i++) {
-//   if inputArray[i].includes(1)
-//     inputArray[i] = "Beep!"
-//   });
-//   console.log(inputArray);
-// };
