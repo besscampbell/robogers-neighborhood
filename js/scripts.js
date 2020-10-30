07c96de1797055bcaa542f51cbd28303059060bf
Bess Campbell <bess.k.campbell@gmail.com>
2020-10-30 14:56:11 -0700

    Add some UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
-// let inputNumber = $(parseInt("input#number")).val();
-let inputNumber =21
 let inputArray = [];
-let output= " "
-
-for (i = 0; i <= inputNumber; i++) {
+let output
+let inputNumber
+console.log(inputNumber);
+for (i = 0; i <= inputNumber; i++) function array(element){
   inputArray.push(i);
-};
-
+});
 const roboSpeak = inputArray.map(function roboger(number){
   let string = number.toString();
   if (string.includes("3")){ 
@@ -14,10 +12,22 @@ const roboSpeak = inputArray.map(function roboger(number){
   } else if(string.includes("2")){
     output = "Boop!"
   } else if (string.includes("1")){
-      output = "Beep!";
+    output = "Beep!";
   } else {
-      output = number
+    output = number
   }
-    
+  console.log(roboSpeak);
   return output;
+  
+});
+
+ $(document).ready(function(){
+  $("#roboger").submit(function(){
+    let inputNumber = $("input#number").val();
+  });
+  $("#response").text(roboSpeak);
+  console.log(roboSpeak);
+
+  array(inputNumber);
+  roboger(inputArray);
 });
